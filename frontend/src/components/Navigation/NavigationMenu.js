5117d40e2eb820c024f56c8184d98ccd6493760e
MichaelSal <salariosamichael@gmail.com>
2023-12-05 06:10:13 -0800

    added nav bar template

---
+++ frontend/src/components/Navigation/NavigationMenu.js
@@ -0,0 +1,29 @@
+import './index.css';
+
+function NavigationMenu() {
+  return (
+    <div className="nav-menu">
+      <div>
+        <p>Browse</p>
+      </div>
+      
+      <div>
+        <p>Manga</p>
+      </div>
+
+      <div>
+        <p>Games</p>
+      </div>
+
+      <div>
+        <p>Store</p>
+      </div>
+
+      <div>
+        <p>News</p>
+      </div>
+    </div>
+  )
+}
+
+export default NavigationMenu;
---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+import NavigationMenu from './NavigationMenu';
+
+function Navigation() {
+
+  return (
+    <div className='nav-bar'>
+      <div className='nav-logo'>
+        <p>mikeyroll</p>
+      </div>
+      <NavigationMenu/>
+    </div>
+  )
+}
+
+
+export default Navigation;
